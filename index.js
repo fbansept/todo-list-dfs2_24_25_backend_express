302eb65c190f081141535ebe4073f1dfabd6fdd0
fbansept <bansept.franck@gmail.com>
2024-11-28 16:56:58 +0100

    traitement jwt

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const cors = require("cors"); // Importer le middleware CORS
+const jwtUtils = require("jsonwebtoken"); // gestion des jwt
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json()); // for parsing application/json
@@ -25,16 +26,28 @@ app.get("/", (req, res) => {
 });
 
 app.get("/taches", (req, res) => {
-  connection.query("SELECT * FROM tache ", (erreur, lignes, champs) => {
-    if (erreur) throw erreur;
+  const token = req.headers["authorization"];
 
-    res.send(lignes);
+  jwtUtils.verify(token, "azerty", (erreur, donnees) => {
+    if (erreur) return res.sendStatus(403);
+
+    const pseudo = donnees.sub;
+
+    console.log(pseudo);
+
+    connection.query("SELECT * FROM tache ", (erreur, lignes, champs) => {
+      if (erreur) throw erreur;
+
+      res.send(lignes);
+    });
+
   });
+  
 });
 
 app.post("/tache", (req, res) => {
   connection.query(
-    "INSERT INTO tache(texte, fini) VALUES (?, 0)",
+    "INSERT INTO tache(texte, fini, date_creation) VALUES (?, 0, NOW())",
     [req.body.texte],
     (erreur, lignes, champs) => {
       if (erreur) throw erreur;
@@ -46,7 +59,6 @@ app.post("/tache", (req, res) => {
 
 //ajouter une méthode pour supprimer une tache
 app.delete("/tache/:id", (req, res) => {
-  
   const idTache = req.params.id;
 
   connection.query(
@@ -56,6 +68,42 @@ app.delete("/tache/:id", (req, res) => {
       if (erreur) throw erreur;
 
       res.status(200).send({ message: "tache supprimée" });
+    }
+  );
+});
+
+app.patch("/tache/change-status/:id", (req, res) => {
+  const fini = req.body.fini;
+  const idTache = req.params.id;
+
+  connection.query(
+    "UPDATE tache SET fini = ? WHERE id = ?",
+    [fini ? "1" : "0", idTache],
+    (erreur, lignes, champs) => {
+      if (erreur) throw erreur;
+
+      res.status(200).send({ message: "tache modifiée" });
+    }
+  );
+});
+
+app.post("/connexion", (req, res) => {
+  connection.query(
+    "SELECT * FROM utilisateur WHERE pseudo = ? AND mot_de_passe = ?",
+    [req.body.pseudo, req.body.motDePasse],
+    (erreur, lignes, champs) => {
+      if (erreur) throw erreur;
+
+      //si l'utilisateur existe
+      if (lignes.length > 0) {
+        //créer le JWT
+        const jwt = jwtUtils.sign({ sub: req.body.pseudo }, "azerty");
+
+        //retourner le jwt
+        res.send({ jwt });
+      } else {
+        res.status(403).send({ message: "Identifiants invalides" });
+      }
     }
   );
 });
